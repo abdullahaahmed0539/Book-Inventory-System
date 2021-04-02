815ecef5166b2bc92a60c45bc32b21f3b92fdd4f
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2021-04-02 23:26:51 +0500

    class book added

--- InventorySystem.cpp
+++ InventorySystem.cpp
@@ -1,6 +1,40 @@
+#include <string>
 #include <iostream>
 using namespace std;
 
+
+class book{
+public:
+    //constructors
+    book(){
+        price = 0.0;
+        available = false;
+    }
+    book  (
+            string book_title, 
+            string book_author,
+            string book_publisher, 
+            string book_stock_position, 
+            float book_price, 
+            bool book_availability
+            )
+    {
+        title = book_title;
+        author = book_author;
+        publisher = book_publisher;
+        stock_position = book_stock_position;
+        price = book_price;
+        available = book_availability;
+
+    }
+private:
+    string title;
+    string author;
+    string publisher;
+    string stock_position;
+    float price;
+    bool available;
+};
 
 int main (void) {
     return 0;
