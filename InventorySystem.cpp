f83bfb8831145a9def2201aab3d58cfaef42d182
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2021-04-03 00:00:26 +0500

    class employee added

--- InventorySystem.cpp
+++ InventorySystem.cpp
@@ -7,35 +7,113 @@ class book{
 public:
     //constructors
     book(){
+        stockCount = 0;
         price = 0.0;
         available = false;
     }
     book  (
-            string book_title, 
-            string book_author,
-            string book_publisher, 
-            string book_stock_position, 
-            float book_price, 
-            bool book_availability
+            string bookTitle, 
+            string bookAuthor,
+            string bookPublisher, 
+            int bookStockCount, 
+            float bookPrice, 
+            bool bookAvailability
             )
     {
-        title = book_title;
-        author = book_author;
-        publisher = book_publisher;
-        stock_position = book_stock_position;
-        price = book_price;
-        available = book_availability;
+        title = bookTitle;
+        author = bookAuthor;
+        publisher = bookPublisher;
+        stockCount = bookStockCount;
+        price = bookPrice;
+        available = bookAvailability;
 
     }
+
+    //Accessors
+    string getTitle (){
+        return title;
+    }
+
+    string getAuthor (){
+        return author;
+    }
+
+    string getPublisher (){
+        return publisher;
+    }
+
+    int getStockCount (){
+        return stockCount;
+    }
+
+    float getPrice(){
+        return price;
+    }
+
+    bool getAvailability (){
+        return available;
+    }
+
+    //Mutators
+    void setTitle(string bookTitle){title = bookTitle;}
+    void setAuthor(string bookAuthor){author = bookAuthor;}
+    void setPublisher(string bookPublisher){publisher = bookPublisher;}
+    void setStockCount(int bookCount){stockCount = bookCount;}
+    void setprice(float bookPrice){price = bookPrice;}
+    void setAvailability(bool status){available = status;}
+
+
 private:
     string title;
     string author;
     string publisher;
-    string stock_position;
+    int stockCount;
     float price;
     bool available;
 };
 
+class employee{
+    public:
+        //Constructors
+        employee(){
+            salary = 0.0;
+        }
+        employee(string employeeName, string employeeJobTitle, double employeeSalary ){
+            name = employeeName;
+            jobTitle = employeeJobTitle;
+            salary = employeeSalary;
+        }
+
+        //Accessors
+        string getName(){
+            return name;
+        }
+
+        string getJobTitle(){
+            return jobTitle;
+        }
+
+        double getSalary(){
+            return salary;
+        }
+
+        //Mutators
+        void setName(string employeeName){name = employeeName;}
+        void setJobTitle(string employeeJobTitle){jobTitle = employeeJobTitle;}
+        void setSalary(double employeeSalary){salary = employeeSalary;}
+
+    private:
+        string name;
+        string jobTitle;
+        double salary;
+};
+
+
+
+
+
+
 int main (void) {
+
     return 0;
 }
